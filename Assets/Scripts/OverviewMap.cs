c6dd16ad32ab907594a81ccc5a3b1a882c12c618
jdallen1000 <71626143+jdallen1000@users.noreply.github.com>
2022-08-28 21:50:21 +0800

    Pacman game

---
+++ Assets/Scripts/OverviewMap.cs
@@ -0,0 +1,41 @@
+// Jordan Allen
+// 26/8/2022
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverviewMap : MonoBehaviour
+{
+    public GameObject UImap;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UImap.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            //Get current State
+            bool currentState = UImap.activeSelf;
+
+            //Flip it
+            currentState = !currentState;
+
+            //Set the current State to the flipped value
+            UImap.SetActive(currentState);
+        }
+    }
+
+    
+
+
+
+        
+}
